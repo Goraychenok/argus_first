45d88bec0cd4c8267b11c12255aaa9d8e122b5ae
Goraychenok <55753151+Goraychenok@users.noreply.github.com>
2020-09-04 00:55:35 +0500

    Add files via upload

---
+++ js/about.js
@@ -0,0 +1,46 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  $(document).ready(function() {
+
+	$('.image-popup-vertical-fit').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}
+		
+	});
+
+	$('.image-popup-fit-width').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		image: {
+			verticalFit: false
+		}
+	});
+
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+});
--- js/main.js
+++ js/main.js
@@ -4,17 +4,26 @@ jQuery(document).ready(function() {
         $('body').toggleClass('lock');
         $('.header').addClass('active');
     });
-    
-    $('.popup-btn').on('click',  function(event) {
-		event.preventDefault();
-		$('.popup').fadeIn();
-        $('body').addClass('stop');
-        $('.header').addClass('none');
-	}); 
-	$('.popup-close').on('click',  function(event) {
-		event.preventDefault();
-		$('.popup').fadeOut();
-        $('body').removeClass('stop');
-        $('.header').removeClass('none');
-	});
 });
+$('.popup-btn').on('click',  function(event) {
+    event.preventDefault();
+    $('.popup').fadeIn();
+	$('body').addClass('stop');
+	$('.header').addClass('none');
+});
+
+$(document).mouseup(function (e){ // событие клика по веб-документу
+    var popup = $("#popup-dialog"); // тут указываем ID элемента
+    var close = $(".popup-close");
+    $('body').addClass('stop');
+    if (!popup.is(e.target)// если клик был не по нашему блоку
+        && popup.has(e.target).length === 0 // и не по его дочерним элементам
+        || close.is(e.target)) { 
+            $('body').addClass('stop');
+            event.preventDefault();
+            $('.popup').fadeOut();
+			$('body').removeClass('stop');
+			$('.header').removeClass('none');
+    }
+});
+
